34576aaf7460fc2a320dd633927fb4ac64470cb2
seunjin <jinseun@gmail.com>
2026-05-05 11:59:23 +0900

    feat: implement room api

--- apps/server/src/room/dto/update-room.dto.ts
+++ apps/server/src/room/dto/update-room.dto.ts
@@ -1,1 +1,17 @@
-export class UpdateRoomDto {}
+import { IsBoolean, IsOptional, IsString, MaxLength } from 'class-validator';
+
+export class UpdateRoomDto {
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  title?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isPublic?: boolean;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(8)
+  password?: string;
+}
